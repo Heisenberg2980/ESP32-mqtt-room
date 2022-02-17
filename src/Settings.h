2d80bf11638efcda52c454d2e7e9c25007f1e10a
Heisenberg <el_bueno222@hotmail.com>
2022-02-17 21:16:12 +0000

    renamed settings files

--- src/Settings.h
+++
@@ -1,6 +0,0 @@
-//Replace with the room name where the node will be placed; example: #define room "living-room"
-#define room "living-room"
-#define instance "1"
-
-//Replace with your Wifi SSID; example: #define ssid "MyWifi"
-#define ssid "MyWifi"
